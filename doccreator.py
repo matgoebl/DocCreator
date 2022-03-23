4385636baefe9d14e0b05b16dcf21d63647dceb2
Matthias Goebl <matthias.goebl@goebl.net>
2022-03-23 07:59:28 +0100

    Add defaults, Option to set branch prefix

--- doccreator.py
+++ doccreator.py
@@ -14,14 +14,18 @@ import jsonpath_ng
 import re
 import time
 
-BRANCH_PREFIX = "doccreator"
-
 
 class DocCreator:
-    def __init__(self, base_path, template_file):
-        self.__base_path = base_path
+    def __init__(self, repo_path, repo_url, template_file):
+        self.__repo_path = repo_path
+        out = None
+        if not os.path.exists(self.__repo_path) and repo_url:
+            out = cmd(f"git clone {repo_url} {self.__repo_path}")
+        else:
+            out = cmd(f"git -C {self.__repo_path} checkout main && git -C {self.__repo_path} pull || true")
+
         self.name("unknown")
-        template_filename = os.path.join(self.__base_path, template_file)
+        template_filename = os.path.join(self.__repo_path, template_file)
         with open(template_filename) as f:
             self.__doc = json.load(f)
 
@@ -37,37 +41,38 @@ class DocCreator:
         return ",".join([str(match.value) for match in jsonpath_expr.find(self.__doc) if match.value != None])
 
     def name(self, newname, commondir=None):
-        newname = re.sub(r'[^a-zA-Z]', '', newname)
+        newname = re.sub(r'[^a-zA-Z0-9]', '', newname)
         newname = newname[:16]
         self.__name = newname
         self.__commondir = commondir
         if commondir:
-            self.__path = os.path.join(self.__base_path, commondir)
+            self.__path = commondir
         else:
-            self.__path = os.path.join(self.__base_path, self.__name)
-        self.__filename = os.path.join(self.__path, self.__name + ".json")
+            self.__path = self.__name
 
     def write(self):
+        self.__filename = os.path.join(self.__path, self.__name + ".json")
+        fp = os.path.join(self.__repo_path, self.__path)
+        fn = os.path.join(self.__repo_path, self.__filename)
         if self.__commondir:
-            os.makedirs(self.__path, exist_ok=True)
+            os.makedirs(fp, exist_ok=True)
         else:
-            if os.path.exists(self.__path):
+            if os.path.exists(fp):
                 raise Exception(f"Path '{self.__path}' already exists.")
-            os.mkdir(self.__path)
-        if os.path.exists(self.__filename):
+            os.mkdir(fp)
+        if os.path.exists(fn):
             raise Exception(f"File '{self.__filename}' already exists.")
-        with open(self.__filename, 'w') as f:
+        with open(fn, 'w') as f:
             f.write(str(self))
 
-    def pushbranch(self, dryrun=False):
+    def pushbranch(self, branch_prefix, dryrun=False):
         out = []
-        branch = BRANCH_PREFIX + "-" + self.__name + "-" + str(int(time.time()))
-        out.append(cmd(f"git checkout master"))
-        out.append(cmd(f"git checkout -b {branch}"))
-        out.append(cmd(f"git add {self.__filename}"))
-        out.append(cmd(f"git commit -m 'new from template' {self.__filename}"))
+        branch = branch_prefix + "-" + self.__name + "-" + str(int(time.time()))
+        out.append(cmd(f"git -C {self.__repo_path} checkout -b {branch}"))
+        out.append(cmd(f"git -C {self.__repo_path} add {self.__filename}"))
+        out.append(cmd(f"git -C {self.__repo_path} commit -m 'new from template' {self.__filename}"))
         if not dryrun:
-            out.append(cmd(f"git push --set-upstream origin {branch}"))
+            out.append(cmd(f"git -C {self.__repo_path} push --set-upstream origin {branch}"))
         return '\n'.join(out)
 
 
@@ -99,9 +104,11 @@ def main():
         description='Tool to create JSON documents by filling templates and commiting them to git repositories.',
         epilog='For details see https://github.com/matgoebl/DocCreator .')
     parser.add_argument('-p', '--gitrepopath',default=".",               help='Set git repository path.' )
+    parser.add_argument('-u', '--gitrepourl', default=None,              help='Set git repository url.' )
     parser.add_argument('-d', '--commondir',  default=None,              help='Write document to a common directory, otherwise create new directory base on given name.')
+    parser.add_argument('-b', '--branchprefix',default="doccreator",     help='Set branch prefix.' )
     parser.add_argument('-t', '--template',   default='template.json',   help='Set template file name relative to git repository path.')
-    parser.add_argument('-f', '--fields',     default='Name,Details.Location,Details.Type',   help='List of fields to replace, comma separated.')
+    parser.add_argument('-f', '--fields',     default='',                help='List of fields to replace, comma separated.')
     parser.add_argument('-r', '--readfile',   default=None,              help='Read input from file.')
     parser.add_argument('-i', '--interactive',action='store_true',       help='Run creation interactive.' )
     parser.add_argument('-n', '--dryrun',     action='store_true',       help='Do not push branch.' )
@@ -113,7 +120,7 @@ def main():
 
 
     try:
-        doc = DocCreator(args.gitrepopath, args.template)
+        doc = DocCreator(args.gitrepopath, args.gitrepourl, args.template)
         if args.verbose >= 2:
             print(f"Source document:\n{doc}")
 
@@ -125,6 +132,8 @@ def main():
             with open(args.readfile) as f:
                 doc.name(f.read(), args.commondir)
                 for field in args.fields.split(','):
+                    if field.find('=') >= 0:
+                        field,default = field.split('=',2)
                     doc.set(field,f.read())
 
         elif args.interactive:
@@ -134,18 +143,23 @@ def main():
             atexit.register(save_readline_history)
 
             for field in args.fields.split(','):
-                val = input_prefilled(f"{field}: ", doc.get(field))
+                if field.find('=') >= 0:
+                    field,default = field.split('=',2)
+                else:
+                    default = doc.get(field)
+                val = input_prefilled(f"{field}: ", default)
                 doc.set(field,val)
             doc.name(doc.get('Name'))
 
         else:
             sys.exit(0)
 
-        if args.verbose:
-            print(f"Result document:\n{doc}")
+        if args.verbose >= 2:
+            print(f"Result document for {doc.get('Name')}:\n{doc}")
         doc.write()
-        out = doc.pushbranch()
-        print(out)
+        out = doc.pushbranch(args.branchprefix, args.dryrun)
+        if args.verbose:
+            print(out)
 
     except Exception as e:
         print(f"Error: {e}")
